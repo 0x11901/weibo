4301fa5ab2f3878149434e59c43c24f9e0aa3427
王靖凯 <wjk930726@live.com>
2016-11-26 18:58:36 +0800

    create a convenience constructor

---
+++ weiBo/weiBo/Classes/Utilities/Category/UIButton+WJKAddition.swift
@@ -0,0 +1,44 @@
+//
+//  UIButton+WJKAddition.swift
+//  weiBo
+//
+//  Created by 王靖凯 on 2016/11/26.
+//  Copyright © 2016年 王靖凯. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    convenience init(title: String?, fontSize: CGFloat = 13, color: UIColor = UIColor.darkGray,image: String? = nil,bgImage: String? = nil) {
+        self.init()
+        if let title = title {
+            self.setTitle(title, for: .normal)
+            self.setTitleColor(color, for: .normal)
+            self.titleLabel?.font = UIFont.systemFont(ofSize: fontSize)
+        }
+        
+        if let image = image {
+            self.setImage(UIImage(named: image), for: .normal)
+            let hlightImage = "\(image)_hlighted"
+            if let hlightImage = UIImage(named: hlightImage) {
+                self.setImage(hlightImage, for: .highlighted)
+            }
+            let selectedImage = "\(image)_selected"
+            if let selectedImage = UIImage(named: selectedImage) {
+                self.setImage(selectedImage, for: .selected)
+            }
+        }
+        
+        if let bgImage = bgImage {
+            self.setBackgroundImage(UIImage(named: bgImage), for: .normal)
+            let hlightImage = "\(image)_hlighted"
+            if let hlightImage = UIImage(named: hlightImage) {
+                self.setImage(hlightImage, for: .highlighted)
+            }
+            let selectedImage = "\(image)_selected"
+            if let selectedImage = UIImage(named: selectedImage) {
+                self.setImage(selectedImage, for: .selected)
+            }
+        }
+    }
+}
---
+++ weiBo/weiBo/Classes/Utilities/Category/common.swift
@@ -0,0 +1,21 @@
+//
+//  common.swift
+//  weiBo
+//
+//  Created by 王靖凯 on 2016/11/26.
+//  Copyright © 2016年 王靖凯. All rights reserved.
+//
+
+import UIKit
+
+// 全局颜色
+let globalColor = UIColor.orange
+
+// 屏幕的宽度
+let screenWidth = UIScreen.main.bounds.size.width
+
+//屏幕的高度
+let screenHeight = UIScreen.main.bounds.size.height
+
+//分辨率的倍数
+let screenScale = UIScreen.main.scale
