8582a9b354d1956359f06e9e28eb489bd822750d
王靖凯 <wjk930726@live.com>
2016-11-29 18:29:43 +0800

    create few files to set home page

---
+++ weiBo/weiBo/Classes/ViewModel/WBStatusViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WBStatusViewModel.swift
+//  weiBo
+//
+//  Created by 王靖凯 on 2016/11/29.
+//  Copyright © 2016年 王靖凯. All rights reserved.
+//
+
+import UIKit
+
+class WBStatusViewModel: NSObject {
+
+}
